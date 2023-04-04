a4ac04f7998ec10423455ac348a3ef9f9b47c720
kopo12 <shgksthf99@gmail.com>
2023-04-04 11:23:12 +0900

    #으로 변경

--- h.java
+++ h.java
@@ -1,6 +1,6 @@
 public class Helloword{
     public static void main(String[] args){
         System.out.println("Hello");
-        System.out.println("222Hello");
+        System.out.println("222Hello########");
     }
 }
