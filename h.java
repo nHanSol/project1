00519f855c4fd8e4419817f9cf0c1a23a8ab7ac2
kopo12 <shgksthf99@gmail.com>
2023-04-04 11:08:03 +0900

    수정222

---
+++ h.java
@@ -0,0 +1,7 @@
+public class Helloword{
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("222Hello");
+        System.out.println("333Hello");
+    }
+}
